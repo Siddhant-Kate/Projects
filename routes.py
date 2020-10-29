dcb8edd51cd55b124313c97fd7e1b6305a04cacc
the-tunnster <therealtarunkrishnan@gmail.com>
2020-10-29 16:32:49 +0530

    Added CSS Styling, probably ditching jinja.

--- routes.py
+++ routes.py
@@ -21,22 +21,15 @@ def fill():
 def browse():
     return render_template('browse.html')
 
-@app.route('/login', methods = ['GET', 'POST'])
+@app.route('/login')
 def login():
-    if request.method == 'POST':
-        session.pop('user_id', None)
-        username = request.form['username']
-        password = request.form['password']
-
-        user = [x for x in users if x.username == username][0]
-        if user and user.password == password:
-            session['user_id'] = user.id
-            return redirect(url_for('profile'))
-
-        return redirect(url_for('login'))
-
     return render_template('login.html')
 
 @app.route('/profile')
 def profile():
     return render_template('profile.html', user = user)
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
