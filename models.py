aa478f790cd6aa20c8a5e02e9825e85faede0dcc
the-tunnster <therealtarunkrishnan@gmail.com>
2020-11-04 12:11:05 +0530

    Changed database model name and added new columns.

--- models.py
+++ models.py
@@ -1,10 +1,11 @@
 from app import db
 from datetime import datetime
 
-class Task(db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    title = db.Column(db.String(100), nullable = False)
-    date = db.Column(db.Date, nullable = False)
+    username = db.Column(db.String(100), nullable = False)
+    password = db.Column(db.String(100), nullable = False)
+    email = db.Column(db.String(100), nullable = False)
 
     def __repr__(self):
         return f'{self.title} created on {self.date}'
