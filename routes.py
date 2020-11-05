7d3c813279cde2b39e43187d8a02da2a753ac877
the-tunnster <therealtarunkrishnan@gmail.com>
2020-11-05 14:04:56 +0530

    Fixed error with autosubmitting.

--- routes.py
+++ routes.py
@@ -24,17 +24,18 @@ def browse():
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
-    user_name = request.form.get('username')
-    pass_word = request.form.get('password')
+    if request.method == "POST":
+        user_name = request.form.get('username')
+        pass_word = request.form.get('password')
 
-    user_details = User.query.filter(User.username==user_name).first()
+        user_details = User.query.filter(User.username==user_name).first()
 
-    if user_details is None:
-        print("Test line.")
-        return render_template('login.html')
-    
-    if user_details:
-        return render_template('index.html')
+        if user_details is None:
+            print("Test line.")
+            return render_template('login.html')
+            
+        if user_details:
+            return render_template('index.html')
     
     return render_template('login.html')
 
@@ -44,41 +45,42 @@ def profile():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    u_name = request.form.get('username')
-    p_word = request.form.get('password')
-    cp_word = request.form.get('confirm_password')
-    email = request.form.get('email')
-    c_email = request.form.get('confirm_email')
+    if request.method == "POST":
+        u_name = request.form.get('username')
+        p_word = request.form.get('password')
+        cp_word = request.form.get('confirm_password')
+        email = request.form.get('email')
+        c_email = request.form.get('confirm_email')
 
-    if u_name == None or p_word == None or c_email == None:
-        return render_template('register.html')
+        if u_name == None or p_word == None or c_email == None:
+            return render_template('register.html')
 
-    if email != c_email:
-        flash("Your email id is not entered correctly.")
-        return render_template('register.html')
+        if email != c_email:
+            flash("Your email id is not entered correctly.")
+            return render_template('register.html')
 
-    if p_word != cp_word:
-        flash("Your password is not entered correctly.")
-        return render_template('register.html')
+        if p_word != cp_word:
+            flash("Your password is not entered correctly.")
+            return render_template('register.html')
 
-    temp = User.query.filter(User.email==email).first()
-    if temp:
-        flash("This email id is already registered.")
-        return render_template('register.html')
+        temp = User.query.filter(User.email==email).first()
+        if temp:
+            flash("This email id is already registered.")
+            return render_template('register.html')
 
-    temp = User.query.filter(User.username==u_name).first()
-    if temp:
-        flash("This username is already registered.")
-        return render_template('register.html')
+        temp = User.query.filter(User.username==u_name).first()
+        if temp:
+            flash("This username is already registered.")
+            return render_template('register.html')
 
-    new_id = randint(5, 1000)
-    temp = User.query.filter(User.id==new_id).first()
-    if temp is None:
-        new_user = User(id=new_id, username=u_name, password=p_word, email=c_email)
-        db.session.add(new_user)
-        db.session.commit()
-        flash("You have registered successfully.")
-        return render_template('login.html')
+        new_id = randint(5, 1000)
+        temp = User.query.filter(User.id==new_id).first()
+        if temp is None:
+            new_user = User(id=new_id, username=u_name, password=p_word, email=c_email)
+            db.session.add(new_user)
+            db.session.commit()
+            flash("You have registered successfully.")
+            return render_template('login.html')
     
 
     return render_template('register.html')
